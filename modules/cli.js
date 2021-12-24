0f3d1f2a6066dd0818942f24248f39276cee167b
anuvgupta <me@anuv.me>
2021-12-23 22:11:33 -0800

    add typical eval section to cli module for debug purposes

--- modules/cli.js
+++ modules/cli.js
@@ -21,22 +21,29 @@ var init = _ => {
     input.on('line', (line) => {
         line = line.trim();
         if (line != '') {
+            var line_text = `${line}`;
             line = line.split(' ');
             if (line[0] == "testing") {
                 console.log("123");
             } else if (line[0] == "db") {
                 if (line.length > 1 && line[1] == "table") {
-                    if (line.length > 2) {
-                        m.db.example(line[2], result => {
-                            log(result);
-                        });
-                    }
+                    // if (line.length > 2) {
+                    //     m.db.example(line[2], result => {
+                    //         log(result);
+                    //     });
+                    // }
                 }
             } else if (line[0] == "modules") {
                 var output = _util.inspect(m, {
                     showHidden: false, depth: 2, colors: true, compact: false
                 });
                 log(`modules\n${output}`);
+            } else if (line[0] == "code" || line[0] == "eval") {
+                if (line.length > 1 && line[1] != "") {
+                    line_text = line_text.substring(4);
+                    var ret = eval(line_text);
+                    if (ret !== undefined) console.log(ret);
+                }
             } else if (line[0] == "clear" || line[0] == 'c') {
                 console.clear();
             } else if (line[0] == "exit" || line[0] == "quit" || line[0] == 'q') {
