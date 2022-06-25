fbe3279ecf9c94eeed59cda6281375af1cd4ebaf
Anuv <me@anuv.me>
2022-06-25 09:00:11 +0000

    bugfix order

--- config/mongo/backup/mongoexport/mongo-export.sh
+++ config/mongo/backup/mongoexport/mongo-export.sh
@@ -1,4 +1,4 @@
-mongoexport --collection=application --db=dash --out=application.json
 mongoexport --collection=domain --db=dash --out=domain.json
-mongoexport --collection=project --db=dash --out=project.json
 mongoexport --collection=resource --db=dash --out=resource.json
+mongoexport --collection=application --db=dash --out=application.json
+mongoexport --collection=project --db=dash --out=project.json
