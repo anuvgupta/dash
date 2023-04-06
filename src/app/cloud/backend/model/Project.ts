417c714ffcbf1a439b9bee1c6f7559896d7ad2a2
anuvgupta <me@anuv.me>
2023-04-06 01:13:50 -0700

    Update Project model and reorganize backend files

---
+++ src/app/cloud/backend/model/Project.ts
@@ -0,0 +1,76 @@
+/**
+ * Model for Projects
+ */
+class Project {
+    // fields
+    id: string;
+    name: string;
+    slug: string;
+    repo: string;
+    description: string;
+    visible: boolean; // referred to as "public" in the database
+    docs: string;
+    img: string;
+    imgInvert: boolean;
+    icon: string;
+    featured: boolean;
+    applications: string[];
+    link: string;
+    demoPass: string;
+    demoPassShow: boolean;
+    major: boolean;
+    complete: boolean;
+    type: string;
+    platform: string;
+    category: string;
+    purpose: string;
+    tech: {
+        primary: string[];
+        secondary: string[];
+        languages: string[];
+    };
+    tagline: string;
+    tsCreated: number;
+    tsUpdated: number;
+
+    // default constructor
+    constructor(
+        id: string,
+        name: string,
+        slug: string,
+        repo: string,
+        desc: string,
+        visible: boolean
+    ) {
+        const timestamp = Date.now();
+        this.id = id;
+        this.name = name;
+        this.slug = slug;
+        this.repo = repo;
+        this.description = desc;
+        this.visible = visible;
+        this.docs = "";
+        this.img = null;
+        this.imgInvert = false;
+        this.icon = null;
+        this.featured = false;
+        this.applications = [];
+        this.link = "";
+        this.demoPass = "";
+        this.demoPassShow = false;
+        this.major = false;
+        this.complete = false;
+        this.type = "none";
+        this.platform = "none";
+        this.category = "none";
+        this.purpose = "none";
+        this.tech = {
+            primary: [],
+            secondary: [],
+            languages: [],
+        };
+        this.tagline = `${desc.split(" ").splice(0, 5).join(" ")}...`;
+        this.tsCreated = timestamp;
+        this.tsUpdated = timestamp;
+    }
+}
