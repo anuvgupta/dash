2ef33c7416a6bbb63b8534bde709afc97d2db7e0
anuvgupta <me@anuv.me>
2023-04-15 18:40:00 -0700

    Remove comments

--- src/cloud/backend/tst/utils/Log.test.ts
+++ src/cloud/backend/tst/utils/Log.test.ts
@@ -1,17 +1,3 @@
-// import Log from "./backend/utils/Log";
-
-// const log = new Log("main");
-
-// log.info("some info", "abc");
-
-// log.warn("warning 1");
-// log.warn(new Error("error 2 details"));
-// log.warn(new Error("error 3 details"), "warning 3");
-
-// log.error("error 4");
-// log.error(new Error("error 5 details"));
-// log.error(new Error("error 6 details"), "error 6");
-
 import * as Util from "util";
 
 import Log from "../../src/utils/Log";
