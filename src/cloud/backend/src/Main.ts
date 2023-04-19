77005b183a2cc3575cfc1272e68545a26e468a93
anuvgupta <me@anuv.me>
2023-04-18 18:19:17 -0700

    Update package configs and fix build errors

--- src/cloud/backend/src/Main.ts
+++ src/cloud/backend/src/Main.ts
@@ -1,4 +1,5 @@
 import Is from "./utils/Is";
 import Log from "./utils/Log";
 import Utilities from "./utils/Utilities";
+import Configuration from "./config/Configuration";
 import InvalidConfigException from "./exception/InvalidConfigException";
