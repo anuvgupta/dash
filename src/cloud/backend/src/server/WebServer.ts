abe65ab468b56d7d9b85685d06f84ba0fda24eaf
anuvgupta <me@anuv.me>
2023-10-07 12:44:17 -0700

    Organize web server

--- src/cloud/backend/src/server/WebServer.ts
+++ src/cloud/backend/src/server/WebServer.ts
@@ -20,7 +20,7 @@ export default class WebServer {
     backendPath: string;
     frontendPath: string;
     cloudConfig: object;
-    // server: Http.Server;
+    server: Http.Server;
     log: Log;
     constructor(
         port: number,
@@ -35,7 +35,7 @@ export default class WebServer {
         this.backendPath = backendPath;
         this.frontendPath = frontendPath;
         this.cloudConfig = cloudConfig;
-        // this.server = Http.Server(this.api);
+        this.server = null;
     }
 
     load(callback: () => void): WebServer {
@@ -55,16 +55,87 @@ export default class WebServer {
         );
         this.api.use(Express.static(`${this.frontendPath}/assets`));
         this.api.use(Express.static(`${this.frontendPath}/src`));
+        this.api.get("/", this.homeHandler.bind(this));
         // open server
-        this.api.listen(this.port, () => {
+        this.server = this.api.listen(this.port, () => {
             this.log.info("Listening on", this.port);
             callback();
         });
         return this;
     }
 
+    bind(
+        method: string,
+        path: string,
+        handler: (req: Express.Request, res: Express.Response) => void
+    ): void {
+        switch (method) {
+            case "get":
+                this.api.get(path, handler);
+                break;
+            case "post":
+                this.api.post(path, handler);
+                break;
+            default:
+                break;
+        }
+    }
+
     route(): void {
-        this.api.get("/", this.homeHandler.bind(this));
+        this.bind(
+            "get",
+            "/api/projects",
+            (req: Express.Request, res: Express.Response) => {
+                this.returnData(req, res, {
+                    hello: "bye",
+                });
+            }
+        );
+    }
+
+    private homeHandler(req: Express.Request, res: Express.Response): void {
+        this.returnView(req, res, "app", {
+            cloudConfig: this.exportCloudConfig(),
+        });
+    }
+
+    private returnView(
+        req: Express.Request,
+        res: Express.Response,
+        view: string,
+        data: any
+    ): void {
+        res.render(view, data);
+    }
+
+    private returnData(
+        req: Express.Request,
+        res: Express.Response,
+        data: any
+    ): void {
+        res.status(200);
+        res.setHeader("content-type", "application/json");
+        res.send(JSON.stringify(data, null, 2));
+    }
+
+    private returnError(
+        req: Express.Request,
+        res: Express.Response,
+        code: number,
+        msg: string
+    ): void {
+        res.status(code);
+        res.setHeader("content-type", "application/json");
+        res.send(
+            JSON.stringify(
+                {
+                    status: code,
+                    message: msg,
+                },
+                null,
+                2
+            )
+        );
     }
 
     private getCloudConfig(): object {
@@ -78,9 +149,5 @@ export default class WebServer {
             JSON.stringify(this.getCloudConfig())
         ).toString("base64");
         return `JSON.parse(atob("${encodedData}"))`;
-    }
-
-    private homeHandler(req: Express.Request, res: Express.Response): void {
-        res.render("app", { cloudConfig: this.exportCloudConfig() });
     }
 }
